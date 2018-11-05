f6a836af73e7077188e4cd48c5d0a1822a10650f
Teacher <mllib_fiy@163.com>
2018-11-05 16:14:37 +0800

    math工具类

---
+++ src/com/shujia/math/Demo1.java
@@ -0,0 +1,16 @@
+package com.shujia.math;
+
+
+public class Demo1 {
+    public static void main(String[] args) {
+        //绝对值
+        System.out.println(Math.abs(-3.14));
+        System.out.println(Math.max(15, 30));
+        System.out.println(Math.random());
+        System.out.println(Math.random() * 10 + 10);
+        System.out.println(Math.sqrt(16));//平方根
+        System.out.println(Math.round(3.14));
+
+
+    }
+}
