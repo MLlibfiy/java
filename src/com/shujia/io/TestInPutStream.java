38a7415aa21fbcdfeda0f6b1a496ff701c8d2801
Teacher <mllib_fiy@163.com>
2018-11-06 14:10:09 +0800

    字节流批量读取数据

--- src/com/shujia/io/TestInPutStream.java
+++ src/com/shujia/io/TestInPutStream.java
@@ -16,16 +16,23 @@ public class TestInPutStream {
         FileInputStream in = new FileInputStream(file);
         //2、读取数据
         //in.read() 一个一个读取，如果到末尾返回-1
-        int read = in.read();
+        /*int read = in.read();
         while (read != -1) {
             System.out.print((char) read);
             //读取下一个字节
             read = in.read();
-        }
+        }*/
+
+        //一次读取多个字节
+
+        byte[] bytes = new byte[1024];
+        //返回值是这一次读了多少字节
+        int size = in.read(bytes);
+        String s = new String(bytes, 0, size,"GBK");
+        System.out.println(s);
+
         //3、关闭管道
         in.close();
-
-
 
     }
 }
