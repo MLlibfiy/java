90da41ebd9078f6fd04d30a297141c4117336813
Teacher <mllib_fiy@163.com>
2018-11-10 11:22:25 +0800

    文件下载

--- src/com/shujia/socket/TestFIleDownLoadServer.java
+++ src/com/shujia/socket/TestFIleDownLoadServer.java
@@ -6,11 +6,10 @@ import java.net.Socket;
 
 /**
  * 文件下载，服务端
- *
  */
 public class TestFIleDownLoadServer {
 
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args) throws Exception {
         //获取文件列表
         File file = new File("E:\\download");
         File[] files = file.listFiles();
@@ -18,7 +17,7 @@ public class TestFIleDownLoadServer {
 
         ServerSocket serverSocket = new ServerSocket(8888);
         System.out.println("服务端已启动......");
-        while (true){
+        while (true) {
             Socket socket = serverSocket.accept();
 
             /**
@@ -38,6 +37,8 @@ public class TestFIleDownLoadServer {
             ObjectInputStream inputStream = new ObjectInputStream(socket.getInputStream());
             File file1 = (File) inputStream.readObject();
 
+            System.out.println(socket.getInetAddress() + ":正在下载：" + file1.getName());
+
             //根据客户端发送过来的file对象去磁盘读取文件
             FileInputStream fileInputStream = new FileInputStream(file1);
             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
@@ -48,7 +49,7 @@ public class TestFIleDownLoadServer {
 
             //边读边写，详单与通过服务端，将客户端和磁盘连接在一起
             int read = 0;
-            while ((read = bufferedInputStream.read())!=-1){
+            while ((read = bufferedInputStream.read()) != -1) {
                 bufferedOutputStream.write(read);
                 bufferedOutputStream.flush();
             }
@@ -58,6 +59,8 @@ public class TestFIleDownLoadServer {
             fileInputStream.close();
             socket.shutdownOutput();
             socket.shutdownInput();
+
+            System.out.println(socket.getInetAddress() + ":下载已完成" );
 
         }
     }
