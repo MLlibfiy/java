f64475f845728ebd5005c47cc8b222d237192c72
Teacher <mllib_fiy@163.com>
2018-11-12 15:46:23 +0800

    同步方法

--- src/com/shujia/thread/Demo6.java
+++ src/com/shujia/thread/Demo6.java
@@ -19,21 +19,28 @@ class TicketTest implements Runnable {
 
     @Override
     public void run() {
+
         while (true) {
-            //同步代码块，同一时刻只能有一个线程执行，其他线程处于阻塞状态
-            synchronized (this) {
-                if (ticket > 0) {
-                    try {
-                        Thread.sleep(1);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    Thread thread = Thread.currentThread();
-                    ticket--;
-                    System.out.println(thread.getName() + ";" + ticket);
-                }
-            }
+            stat();
         }
 
     }
+
+    /**
+     * 同步方法，将当前对象锁住 ，和同步代码块效果一样
+     *
+     */
+    public synchronized void stat(){
+        if (ticket > 0) {
+            try {
+                Thread.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            Thread thread = Thread.currentThread();
+            ticket--;
+            System.out.println(thread.getName() + ";" + ticket);
+        }
+    }
+
 }
